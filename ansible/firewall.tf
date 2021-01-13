024dc9d55c581cf873a944c468ce62e8f14d32a5
Davide staffieri <69149868+dstf@users.noreply.github.com>
2021-01-13 21:40:00 +0000

    Create firewall.tf

---
+++ ansible/firewall.tf
@@ -0,0 +1,27 @@
+
+---
+- name: Webserver installation
+  hosts: machines
+#privatest.org
+ # remote_user: root
+  become: true
+  vars:
+hostname: ubun    
+servername: ubun
+# ssh_keys:
+ #          -  ssh-rsa AAAA..
+
+
+  tasks:
+    - name: Install UFW
+      apt:
+        name: ufw
+        state: present
+    - name: Setup rules
+      command: "{{ item }}"
+      with_items:
+        - ufw default deny incoming
+        - ufw allow ssh
+        - ufw allow http
+        - ufw allow https
+        - ufw --force enable
