ecdc401d1ce2b032b1b295dc6804a6d871a089bf
Davide staffieri <69149868+dstf@users.noreply.github.com>
2021-04-16 18:47:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ terraform plan -out=tfplan
 terraform apply "tfplan" 
 
 ansible-playbook webserver001.yml -i hosts
-ansible-playbook webserver002.yml  -i hosts
+ansible-playbook webserver002.yml -i hosts
 ansible-playbook haproxy-install.yml -i hosts
