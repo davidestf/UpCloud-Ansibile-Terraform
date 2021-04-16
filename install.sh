8190aa47ada96c1169f82b0e75a452a6849affa0
Davide staffieri <69149868+dstf@users.noreply.github.com>
2021-04-16 23:38:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ terraform init -input=false
 terraform plan -out=tfplan
 terraform apply "tfplan" 
 
-ansible-playbook webserver001.yml -i hosts
-ansible-playbook webserver002.yml -i hosts
-ansible-playbook haproxy-install.yml -i hosts
+ansible-playbook webserver001.yml -i hosts -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-playbook webserver002.yml -i hosts -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-playbook haproxy-install.yml -i hosts -e 'ansible_python_interpreter=/usr/bin/python3'
